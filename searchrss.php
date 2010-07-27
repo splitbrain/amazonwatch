640c1dfcbb36bf09c92501d9b745da55c0875eac
Andreas Gohr <andi@splitbrain.org>
2010-07-27 23:04:44 +0200

    fixed item date in rss

--- searchrss.php
+++ searchrss.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 $PDO = new PDO('sqlite:database.sqlite');
-$stmt_srch = $PDO->prepare('SELECT * FROM search_results WHERE sid = ?');
+$stmt_srch = $PDO->prepare('SELECT *, strftime(\'%s\',dt) as dt FROM search_results WHERE sid = ? ORDER BY dt DESC');
 $stmt_srch->execute(array((int) $_REQUEST['sid']));
 
 
@@ -13,15 +13,13 @@ echo '   <channel>';
 echo '      <title>Amazon Watch</title>';
 echo '      <link>http://fixme/</link>';
 echo '      <description>Monitor Amazon Product Availability</description>';
-echo '      <pubDate>Tue, 10 Jun 2003 04:00:00 GMT</pubDate>';
-echo '      <lastBuildDate>Tue, 10 Jun 2003 09:41:01 GMT</lastBuildDate>';
 
 while($row = $stmt_srch->fetch(PDO::FETCH_ASSOC)){
     echo '<item>';
     echo '<title>'.htmlspecialchars($row['price'].' - '.$row['title']).'</title>';
     echo '<link>'.$row['url'].'</link>';
     echo '<description>'.htmlspecialchars('<img src="'.$row['image'].'" align="left" /> '.$row['more']).'</description>';
-    echo '<pubDate>Tue, 03 Jun 2003 09:39:21 GMT</pubDate>';
+    echo '<pubDate>'.date('r',$row['dt']).'</pubDate>';
     echo '<guid>'.$row['url'].'</guid>';
     echo '</item>';
 }
