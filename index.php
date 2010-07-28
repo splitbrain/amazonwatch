95bfdd91b584787b45928c666ee9438775ca5d89
Andreas Gohr <andi@splitbrain.org>
2010-07-28 20:50:48 +0200

    first simple user interface added

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+$PDO = new PDO('sqlite:database.sqlite');
+header('Content-Type: text/html; charset=utf-8');
+?>
+<html>
+<head>
+    <title>Amazon Watch</title>
+</head>
+<body>
+
+<form action="" method="post">
+    <fieldset>
+        <legend>Monitor Search Results</legend>
+        <label for="query">Search for</label>
+        <input type="text" name="query" id="query" />
+
+        <label for="region">at</label>
+        <select name="region" id="region">
+            <option value="com">Amazon.com</option>
+            <option value="de">Amazon.de</option>
+            <option value="co.uk">Amazon.co.uk</option>
+            <option value="fr">Amazon.fr</option>
+        </select>.<br />
+
+        <input type="submit" value="Create Monitor Feed" />
+    </fieldset>
+</form>
+
+<?php
+if($_REQUEST['query']){
+    $ins = $PDO->prepare('INSERT INTO search (query,region) VALUES (?,?)');
+    $ins->execute(array($_REQUEST['query'],$_REQUEST['region']));
+    $sid = $PDO->lastInsertId();
+
+    echo '<p>Your search term will now be monitored for three months.
+             All found products will be added to the following RSS feed:</p>';
+
+    echo '<a href="searchrss.php?sid='.$sid.'" target="_blank">Amazon Watch Feed</a>';
+
+    echo '<p>Please subscribe to the feed to be informed when Amazon adds new
+             products matching your query.</p>';
+}
+?>
+
+</body>
