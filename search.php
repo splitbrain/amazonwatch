9da94714c1479a156f2adea59318dbc15a2aef4f
Andreas Gohr <andi@splitbrain.org>
2010-07-28 21:07:11 +0200

    stop fetching outdated monitors

--- search.php
+++ search.php
@@ -7,14 +7,22 @@ $PDO = new PDO('sqlite:database.sqlite');
 $AMZ = new Amazon($CONF['public_key'],$CONF['private_key']);
 
 
-$stmt_srch = $PDO->prepare('SELECT * FROM search');
+$stmt_srch = $PDO->prepare(
+    "SELECT *
+       FROM search
+      WHERE added > datetime('NOW','-90 days')
+        AND lastget > datetime('NOW','-5 days')"
+);
 $stmt_ins  = $PDO->prepare(
-'INSERT OR IGNORE INTO search_results
-        (sid, asin, title, price, url, image, more)
- VALUES (?,?,?,?,?,?,?)');
+    'INSERT OR IGNORE INTO search_results
+            (sid, asin, title, price, url, image, more)
+     VALUES (?,?,?,?,?,?,?)'
+);
 
 $stmt_srch->execute();
 while($row = $stmt_srch->fetch(PDO::FETCH_ASSOC)){
+    printf("%5s %s\n",$row['region'],$row['query']);
+
     $AMZ->setRegion($row['region']);
     $items = $AMZ->search($row['query']);
 
