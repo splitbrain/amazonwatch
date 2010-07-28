e06a0e2c3395a3ed1914247fda42875b721e4876
Andreas Gohr <andi@splitbrain.org>
2010-07-28 21:35:19 +0200

    some more info in the RSS feed

--- searchrss.php
+++ searchrss.php
@@ -1,18 +1,34 @@
 <?php
 require_once(dirname(__FILE__).'/inc/init.php');
 
-$stmt_srch = $PDO->prepare('SELECT *, strftime(\'%s\',dt) as dt FROM search_results WHERE sid = ? ORDER BY dt DESC');
+$stmt_srch = $PDO->prepare(
+    'SELECT *, strftime(\'%s\',added) as added
+       FROM search
+      WHERE sid = ?'
+);
 $stmt_srch->execute(array((int) $_REQUEST['sid']));
-
+$info = $stmt_srch->fetch(PDO::FETCH_ASSOC);
+if(!$info){
+    header("HTTP/1.0 404 Not Found");
+    echo 'No such monitoring';
+    exit;
+}
 
 header('Content-Type:  text/xml; charset=utf-8');
-
 echo '<?xml version="1.0"?>';
 echo '<rss version="2.0">';
 echo '   <channel>';
-echo '      <title>Amazon Watch</title>';
+echo '      <title>'.htmlspecialchars($info['query']).' at Amazon.'.htmlspecialchars($info['region']).'</title>';
 echo '      <link>http://fixme/</link>';
-echo '      <description>Monitor Amazon Product Availability</description>';
+echo '      <description>Monitoring Amazon Product Availability</description>';
+
+$stmt_srch = $PDO->prepare(
+    'SELECT *, strftime(\'%s\',dt) as dt
+       FROM search_results
+      WHERE sid = ?
+   ORDER BY dt DESC'
+);
+$stmt_srch->execute(array((int) $_REQUEST['sid']));
 
 while($row = $stmt_srch->fetch(PDO::FETCH_ASSOC)){
     echo '<item>';
